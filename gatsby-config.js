2ba2de50ca4c88ed5ec6311d186c33ac3f5b46c5
Nurai <coder.nurai@gmail.com>
2024-12-25 20:54:46 +0530

    fixed path

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: 'Nurai',
     description:
       'I’m a software engineer specializing in building web-based applications.',
-    siteUrl: 'https://www.nurai.dev', // No trailing slash allowed!
+      siteUrl: 'https://dev-nurai.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
